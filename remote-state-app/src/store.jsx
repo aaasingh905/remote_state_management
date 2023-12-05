ddb8d13bf9049194846ce430577fc8fc73ae174a
Anurag Singh <anusingh29@WKMZT9AEC488.global.publicisgroupe.net>
2023-12-05 15:23:25 +0530

    adding api call example

--- remote-state-app/src/store.jsx
+++ remote-state-app/src/store.jsx
@@ -2,9 +2,9 @@ import create from "zustand";
 import useIncrementStore from "./store/incrementStore";
 import useApiDataStore from "./store/apiDataStore";
 
-export const useBoundStore = create((...a) => ({
+export const useStore = create((...a) => ({
   ...useIncrementStore(...a),
   ...useApiDataStore(...a),
 }));
 
-export default useBoundStore;
+export default useStore;
